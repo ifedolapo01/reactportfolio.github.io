326ce5b6a0ba0dda9a7247817733b3149418dfd1
Ifedolapo Ajayi <ifedolapoajayi0@gmail.com>
2022-03-31 23:19:27 +0100

    first commit

---
+++ client/src/PortfolioContainer/Home/Profile/Profile.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable no-undef */
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+import Typical from "react-typical";
+import ScrollService from "../../../utilities/ScrollService";
+
+import "./Profile.scss";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="#top">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="#top">
+                <i className="fa fa-google-plus-square"></i>
+              </a>
+              <a href="#top">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="#top">
+                <i className="fa fa-youtube-square"></i>
+              </a>
+              <a href="#top">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Seun</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Enthusiastic Dev✌",
+                    1000,
+                    "UI/UX Developer💻",
+                    1000,
+                    "Microsoft Office Expert😎",
+                    1000,
+                    "Problem Solver🌟",
+                    1000,
+                  ]}
+                />
+              </h1>
+            </span>
+            <span className="profile-role-tagline">
+              Knack for designing websites and applications with UI/UX
+              operations.
+            </span>
+          </div>
+          <div className="profile-options">
+            <button
+              className="btn primary-btn"
+              onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+            >
+              Hire Me
+            </button>
+            <a href="SeunCV.pdf" download="Seun's CV.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/PortfolioContainer/Testimonial/Testimonial.jsx
@@ -0,0 +1,217 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./Testimonial.scss";
+import lady from "../../../src/img/Testimonial/lady.png";
+import mike from "../../../src/img/Testimonial/mike.png";
+import man from "../../../src/img/Testimonial/man.png";
+import ife from "../../../src/img/Testimonial/ife.jpg";
+import shape from "../../../src/img/Testimonial/shape-bg.png";
+
+export default function Testimonial(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const options = {
+    loop: true,
+    margin: 0,
+    nav: true,
+    animateIn: "bounceInRight",
+    animateOut: "bounceOutRight",
+    dots: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ScreenHeading
+        title={"Testimonial"}
+        subHeading={"What My Clients Say About Me"}
+      />
+      <section className="testimonial-section fade-in" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <OwlCarousel
+              className="owl-carousel"
+              id="testimonial-carousel"
+              {...options}
+            >
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />I patronized Oluwaseun
+                      and when she delivered, I honestly fell in love with the
+                      project. She is a very honest lady and she delivers on
+                      time.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src={lady} alt="no internet connection" />
+                    <h5>Daisy Dominic</h5>
+                    <p>CEO InansGlobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />I had the pleasure of
+                      working with Oluwaseun on a project and she is undoubtedly
+                      the best UI/UX designer out there. She was able to take my
+                      vision and make it come to life in a way that I never
+                      thought possible. I would recommend her to any company
+                      looking for a creative genius!
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src={mike} alt="no internet connection" />
+                    <h5>Mikel</h5>
+                    <p>CEO MikeTech</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I've known Oluwaseun for over a year now and she is
+                      without a doubt the best designer I have met. Her
+                      expertise in UX design & UI design is truly unmatched. She
+                      has helped me tremendously in my startup and I can't
+                      imagine going forward without her.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src={ife} alt="no internet connection" />
+                    <h5>Ifedolapo Ajayi</h5>
+                    <p>CEO NinjaStoresNG</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      Oluwaseun has been an absolute joy to work with. She is
+                      very creative and always comes up with the best design
+                      ideas. She is also very responsive to feedback and always
+                      makes sure that we are on the same page before moving
+                      forward. I would highly recommend her as a UI/UX designer!
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src={man} alt="no internet connection" />
+                    <h5>Tom Green</h5>
+                    <p>CEO GreenHousery</p>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          </div>
+        </div>
+      </section>
+      <div className="footer-image">
+        <img src={shape} alt="this is not responding" />
+      </div>
+    </div>
+  );
+}
